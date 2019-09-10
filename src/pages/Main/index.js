b3e778fc718afd6e819e2fe1441a38bdb6181201
rurbano3d <rurbano3d@gmail.com>
2019-09-10 08:34:22 -0300

    start project with initial routes

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Container } from './styles';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Main extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Main</h1>
+      </Container>
+    );
+  }
+}
